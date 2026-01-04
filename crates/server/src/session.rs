117d31df23d75f7cede2c0de6e671fb73e3e27ee
mbecker20 <becker.maxh@gmail.com>
2026-01-04 14:53:43 -0800

    mogh_server 0.1.3 remove host_url from session

--- crates/server/src/session.rs
+++ crates/server/src/session.rs
@@ -2,7 +2,7 @@ use tower_sessions::{
   Expiry, MemoryStore, SessionManagerLayer,
   cookie::{SameSite, time::Duration},
 };
-use url::Url;
+use tracing::warn;
 
 pub use tower_sessions::Session;
 
@@ -10,8 +10,10 @@ pub trait SessionConfig {
   fn expiry_seconds(&self) -> i64 {
     60
   }
+  fn host_env_field(&self) -> &str {
+    "HOST"
+  }
   fn host(&self) -> &str;
-  fn host_url(&self) -> Option<&Url>;
 }
 
 /// Adds an in memory session manager layer.
@@ -21,17 +23,27 @@ pub trait SessionConfig {
 pub fn layer(
   config: impl SessionConfig,
 ) -> SessionManagerLayer<MemoryStore> {
+  let host = config.host();
   let mut layer = SessionManagerLayer::new(MemoryStore::default())
     .with_expiry(Expiry::OnInactivity(Duration::seconds(
       config.expiry_seconds(),
     )))
-    .with_secure(config.host().starts_with("https://"))
+    .with_secure(host.starts_with("https://"))
     // Needs Lax in order for sessions to work
     // accross oauth redirects.
     .with_same_site(SameSite::Lax);
-  if let Some(domain) = config.host_url().and_then(|url| url.domain())
+  let host_url = url::Url::parse(host)
+    .inspect_err(|e| {
+      warn!(
+        "Invalid {}: not URL. Passkeys won't work. | {e:?}",
+        config.host_env_field(),
+      )
+    })
+    .ok();
+  if let Some(domain) =
+    host_url.and_then(|url| url.domain().map(str::to_string))
   {
-    layer = layer.with_domain(domain.to_string());
+    layer = layer.with_domain(domain);
   }
   layer
 }
