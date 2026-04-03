32150dd8cde69a8a09a3ea6485f028fe846a9ed0
mbecker20 <becker.maxh@gmail.com>
2026-04-03 11:21:40 -0700

    add section

--- ui/src/index.ts
+++ ui/src/index.ts
@@ -4,3 +4,4 @@ export * from "./components/copy-text";
 export * from "./components/divided-children";
 export * from "./components/enable-switch";
 export * from "./components/mobile-friendly-tabs";
+export * from "./components/section";
